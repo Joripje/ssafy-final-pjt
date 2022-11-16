bc2bf16758711933356ea7e1fb0e9799e6fbf91a
Jaegyeong Jo <89237865+Joripje@users.noreply.github.com>
2022-11-16 17:15:41 +0900

    Detail 요청 API 추가

--- final-pjt-back/movies/views.py
+++ final-pjt-back/movies/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
+from .models import *
+from .serializers import *
 # Create your views here.
+
+
+@api_view(['GET'])
+def movie_detail(request, pk):
+    movie = Movie.objects.get(pk=pk)
+    serializer = MovieSerializer(movie)
+    return Response(serializer.data)
