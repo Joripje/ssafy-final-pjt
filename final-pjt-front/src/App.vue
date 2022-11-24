57f1e1b5e1936dfb5d5bf219417b7aa5e5354cea
dayong2eyo <109324544+dayong2eyo@users.noreply.github.com>
2022-11-25 00:49:42 +0900

    1124

--- final-pjt-front/src/App.vue
+++ final-pjt-front/src/App.vue
@@ -1,65 +1,52 @@
 <template>
+
   <div id="app">
-    <v-app id="inspire" style="height: 50px;">
+    <v-app id="inspire" style="height: 100px;">
       <v-card style="height: 100px;">
         <v-app-bar
           absolute
           color="red"
-          dark
-          scroll-target="#scrolling-techniques-2"
-          style="height: 50px;"
+          style="height: 100px;"
+          src="../assets/navbar.png"
         >
-    
-          <v-app-bar-title style="height: 30px;">나의PS파트너</v-app-bar-title>
-    
+          <img @click="$router.push('/')" class="mr-3 mt-9" :src="require('@/assets/navbar.png')" height="100"/>
           <v-spacer></v-spacer>
     
-          <v-form @submit.prevent="search">
-      <v-container>
-        <v-row>         
-          <v-col
-            cols="12"
-            sm="12"
-            md="12"
-          >
-            <v-text-field
-              v-model="inputData"
-              label="검색"
-              append-icon="mdi-magnify"
-            ></v-text-field>
- 
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-form>
-          <!-- <v-btn icon @click="$router.push('/search')">
-            <v-icon>mdi-magnify</v-icon>
-          </v-btn> -->
-    
-          <v-btn icon @click="$router.push('/')">
+          <v-form @submit.prevent="search" class="mt-12">
+            <v-container>
+              <v-row>         
+                <v-col
+                  cols="12"
+                  sm="12"
+                  md="12"
+                >
+                  <v-text-field
+                    v-model="inputData"
+                    label="검색"
+                    append-icon="mdi-magnify"
+                  ></v-text-field>
+      
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-form>
+          <v-btn icon @click="$router.push('/')" class="mt-9">
             <v-icon>mdi-home</v-icon>
           </v-btn>
     
-          <v-btn v-if="isLoggedIn === true" icon @click="$router.push('/profile')">
+          <v-btn v-if="isLoggedIn === true" icon @click="$router.push('/profile')" class="mt-9">
             <v-icon>mdi-account-box</v-icon>
           </v-btn>
 
-          <v-btn v-if="isLoggedIn === false" icon @click="$router.push('/login')">
+          <v-btn v-if="isLoggedIn === false" icon @click="$router.push('/login')" class="mt-9">
             <v-icon>mdi-login</v-icon>
           </v-btn>
 
-          <v-btn v-if="isLoggedIn === true" icon @click="logOut">
+          <v-btn v-if="isLoggedIn === true" icon @click="logOut" class="mt-9">
             <v-icon>mdi-logout</v-icon>
           </v-btn>
 
         </v-app-bar>
-        <v-sheet
-          id="scrolling-techniques-2"
-          class="overflow-y-auto"
-          max-height="600"
-        >
-          <v-container style="height: 50px;"></v-container>
-        </v-sheet>
       </v-card>
     </v-app>
     <router-view/>
