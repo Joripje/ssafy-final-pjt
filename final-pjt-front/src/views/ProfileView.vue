6d5c1ae4dc6c4b6e0ee0bf79a06429dc347799ab
Jaegyeong Jo <89237865+Joripje@users.noreply.github.com>
2022-11-25 03:24:48 +0900

    프로필 사진

--- final-pjt-front/src/views/ProfileView.vue
+++ final-pjt-front/src/views/ProfileView.vue
@@ -67,21 +67,20 @@ export default {
       return this.$store.state.user_info
     },
     movieImage() {
-      if (this.$store.state.user_review) {
       return 'https://image.tmdb.org/t/p/original' + this.$store.state.user_review[0].movie.backdrop_path
-      } else {
-        return 'https://image.tmdb.org/t/p/original/bQXAqRx2Fgc46uCVWgoPz5L5Dtr.jpg'
+      // } else {
+      //   return 'https://image.tmdb.org/t/p/original/bQXAqRx2Fgc46uCVWgoPz5L5Dtr.jpg'
         }
-    }
+    
   },
   methods: {
-  //   movieImage() {
-  //     console.log(this.$store.state.user_review)
-  //     const BASE_URL = 'https://image.tmdb.org/t/p/original'
-  //     const poster_path = this.$store.state.user_review[0].movie.backdrop_path
+    // movieImage() {
+    //   console.log(this.$store.state.user_review)
+    //   const BASE_URL = 'https://image.tmdb.org/t/p/original'
+    //   const poster_path = this.$store.state.user_review[0].movie.backdrop_path
       
-  //     return BASE_URL + poster_path
-  //   },
+    //   return BASE_URL + poster_path
+    // },
   }
 }
 </script>
